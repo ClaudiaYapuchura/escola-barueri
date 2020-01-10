d5dc816778a202e350c48890aa128d6900b24cca
Claudia Yapuchura <claudiasaire213@gmail.com>
2020-01-10 04:21:48 -0300

    Criar cadastro escola-barueri

---
+++ alunos/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+# Create your models here.
+class Aluno(models.Model):
+    nome = models.CharField(max_length=255)
+    idade = models.IntegerField()
+    
+    generos = (
+        ('F','Feminino'),
+        ('M','Masculino'),
+        ('NB','Não Binário'),
+    )
+
+    genero = models.CharField(max_length=3, choices=generos)
+    nome_mae = models.CharField(max_length=255)
+    nome_pai = models.CharField(max_length=255)
+    endereco = models.CharField(max_length=255)
+    telefone_contato = models.CharField(max_length=13)
